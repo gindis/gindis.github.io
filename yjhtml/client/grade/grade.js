ac99902bbef206f3f177792a0b023f35db3c72a0
gindis.wx <gindis.wx@alibaba-inc.com>
2015-10-09 00:45:14 +0800

    ci

--- yjhtml/client/grade/grade.js
+++ yjhtml/client/grade/grade.js
@@ -1,71 +1,71 @@
 define(function(require, exports, module) {
 
-    'use strict';
+  'use strict';
 
-    var $ = require('zepto');
-    var Flipsnap = require('flipsnap');
-    var evtClick = 'click';
-    if ($.os.phone) {
-        evtClick = 'tap';
-    }
+  var $ = require('zepto');
+  var Flipsnap = require('flipsnap');
+  var evtClick = 'click';
+  if ($.os.phone) {
+    evtClick = 'tap';
+  }
 
-    function G() {
-        this._init.apply(this, arguments)
-    }
+  function G() {
+    this._init.apply(this, arguments)
+  }
 
-    G.prototype = {
-        _slide: function() {
-            if ($('#J_slide').length <= 0) return;
-            var w = window.innerWidth;
-            var $pointer = $('#J_pointer span');
-            var $items = $('#J_slide .span-img-item');
-            if ($items.length <= 1) return;
-            $items.width(w);
-            $('#J_slide').width(w * $items.length);
-            window.flipsnap = Flipsnap('#J_slide', {
-                distance: window.innerWidth,
-                transitionDuration: 100
-            });
-            flipsnap.element.addEventListener('fstouchstart', function(ev) {
-                //$('#J_myfri').css('display', 'none');
-            }, false);
-            flipsnap.element.addEventListener('fspointmove', function() {
-                $pointer.filter('.current').removeClass('current');
-                $pointer.eq(flipsnap.currentPoint).addClass('current');
-            }, false);
-        },
-        _openFix: function () {
-            var $Jfix = $('#J_fix');
-            if ($Jfix.length <=0)return;
-            var $JfixCon = $('#J_fixCon');
-            $Jfix.on(evtClick, function () {
-                var parent = $Jfix.parent();
-                var $b = $(this).find('b');
-                if (!parent.hasClass('open')) {
-                    $JfixCon.animate({
-                        height: '.83rem',
-                        opacity: 1
-                    }, 'ease-in-out', function(){
+  G.prototype = {
+    _slide: function() {
+      if ($('#J_slide').length <= 0) return;
+      var w = window.innerWidth;
+      var $pointer = $('#J_pointer span');
+      var $items = $('#J_slide .span-img-item');
+      if ($items.length <= 1) return;
+      $items.width(w);
+      $('#J_slide').width(w * $items.length);
+      window.flipsnap = Flipsnap('#J_slide', {
+        distance: window.innerWidth,
+        transitionDuration: 100
+      });
+      flipsnap.element.addEventListener('fstouchstart', function(ev) {
+        //$('#J_myfri').css('display', 'none');
+      }, false);
+      flipsnap.element.addEventListener('fspointmove', function() {
+        $pointer.filter('.current').removeClass('current');
+        $pointer.eq(flipsnap.currentPoint).addClass('current');
+      }, false);
+    },
+    _openFix: function () {
+      var $Jfix = $('#J_fix');
+      if ($Jfix.length <=0)return;
+      var $JfixCon = $('#J_fixCon');
+      $Jfix.on(evtClick, function () {
+        var parent = $Jfix.parent();
+        var $b = $(this).find('b');
+        if (!parent.hasClass('open')) {
+          $JfixCon.animate({
+            height: '.83rem',
+            opacity: 1
+          }, 'ease-in-out', function(){
 
-                    });
-                    $b.attr('style', 'transform:rotate(-180deg)');
-                    parent.addClass('open');
-                } else {
-                    $JfixCon.animate({
-                        height: 0,
-                        opacity: 0
-                    }, 'ease-in-out');
-                    $b.attr('style', '');
-                    parent.removeClass('open');
-                }
-            });
-        },
-        _init: function() {
-            var self = this;
-            self._slide();
-            self._openFix();
+          });
+          $b.attr('style', 'transform:rotate(-180deg)');
+          parent.addClass('open');
+        } else {
+          $JfixCon.animate({
+            height: 0,
+            opacity: 0
+          }, 'ease-in-out');
+          $b.attr('style', '');
+          parent.removeClass('open');
         }
-    };
+      });
+    },
+    _init: function() {
+      var self = this;
+      self._slide();
+      self._openFix();
+    }
+  };
 
-    module.exports = new G();
+  module.exports = new G();
 });
---
+++ yjhtml/client/iron/iron.js
@@ -0,0 +1,64 @@
+define(function(require, exports, module) {
+
+  'use strict';
+
+  var $ = require('zepto');
+  var evtClick = 'click';
+  var Scratch = require('module/scratch');
+  if ($.os.phone) {
+    evtClick = 'tap';
+  }
+
+  function I() {
+    this._init.apply(this, arguments)
+  }
+
+  I.prototype = {
+    _resetFont: function () {
+      var a = window,
+        b = a.innerWidth <= 320 ? 320 : a.innerWidth,
+        c = 100 * (b / 320);
+      document.documentElement.style.fontSize = c + "px";
+    },
+    initScratch: function() { // 初始化刮奖
+      var that = this;
+      var w = $('#iron').width();
+      var scratch = new Scratch('iron', ASSET + 'img/iron/start@2x.png', 'image', w, w, drawPercent);
+      scratch.init(ASSET + 'img/iron/thanks@2x.png', 'image');
+
+      function drawPercent(percent) {
+        if (percent == 100) {
+          //scratch.init(that.getRandomTicket(), 'image');
+        }
+      }
+      document.getElementById('freshBtn').onclick = function() {
+        scratch.init(that.getRandomTicket(), 'image');
+      }
+    },
+    getRandomTicket: function () { // 随机出奖
+      var ticketImg = [ASSET + 'img/iron/thanks@2x.png'];
+      //, ASSET + 'img/iron/helmet@2x.png'
+      console.log(Math.floor(Math.random() * ticketImg.length));
+      return ticketImg[Math.floor(Math.random() * ticketImg.length)]
+    },
+    _init: function() {
+      var that = this;
+      // window.onresize = function () {
+      //   $('#iron').html('');
+      //   that._resetFont();
+      //   that.initScratch();
+      // }
+      setTimeout(function() {
+        that._resetFont();
+        setTimeout(function(){
+          $('.wrap').show();
+          that.initScratch();
+          
+        }, 20)
+        
+      }, 0)
+    }
+  };
+
+  module.exports = new I();
+});
